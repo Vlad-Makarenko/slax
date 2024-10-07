5648ff0beabd56d6c260e8421bbafca34eb7fdab
Vlad-Makarenko <makrenkovladok@gmail.com>
2024-10-07 19:09:09 +0300

    show/hide new room modal with live_action

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -35,7 +35,7 @@ defmodule SlaxWeb.ChatRoomLive do
                 <div class="hover:bg-sky-600">
                   <div
                     class="cursor-pointer whitespace-nowrap text-gray-800 hover:text-white px-6 py-1 block"
-                    phx-click={show_modal("new-room-modal")}
+                    phx-click={JS.navigate(~p"/rooms/#{@room}/new")}
                   >
                     Create a new room
                   </div>
@@ -206,7 +206,11 @@ defmodule SlaxWeb.ChatRoomLive do
         </div>
       </div>
     </div>
-    <.modal id="new-room-modal">
+    <.modal
+      id="new-room-modal"
+      show={@live_action == :new}
+      on_cancel={JS.navigate(~p"/rooms/#{@room}")}
+    >
       <.header class="">
         New chat room
       </.header>
