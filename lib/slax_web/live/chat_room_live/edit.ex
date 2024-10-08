d66176597a983dfdfaf48b2eb329d3862a35085e
Vlad-Makarenko <makrenkovladok@gmail.com>
2024-10-08 12:27:38 +0300

    extract SocketHelpers

--- lib/slax_web/live/chat_room_live/edit.ex
+++ lib/slax_web/live/chat_room_live/edit.ex
@@ -28,20 +28,19 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
   def mount(%{"id" => id}, _session, socket) do
     room = Chat.get_room!(id)
 
-    socket =
-      if Chat.joined?(room, socket.assigns.current_user) do
-        changeset = Chat.change_room(room)
+    if Chat.joined?(room, socket.assigns.current_user) do
+      changeset = Chat.change_room(room)
 
-        socket
-        |> assign(page_title: "Edit chat room", room: room)
-        |> assign_form(changeset)
-      else
-        socket
-        |> put_flash(:error, "You are not a member of this room")
-        |> push_navigate(to: ~p"/")
-      end
-
-    {:ok, socket}
+      socket
+      |> assign(page_title: "Edit chat room", room: room)
+      |> assign_form(changeset)
+      |> ok()
+    else
+      socket
+      |> put_flash(:error, "You are not a member of this room")
+      |> push_navigate(to: ~p"/")
+      |> ok()
+    end
   end
 
   def assign_form(socket, %Ecto.Changeset{} = changeset) do
@@ -54,19 +53,19 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
       |> Chat.change_room(room_params)
       |> Map.put(:action, :validate)
 
-    {:noreply, assign_form(socket, changeset)}
+    socket |> assign_form(changeset) |> noreply()
   end
 
   def handle_event("save-room", %{"room" => room_params}, socket) do
     case Chat.update_room(socket.assigns.room, room_params) do
       {:ok, room} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Room updated successfully")
-         |> push_navigate(to: ~p"/rooms/#{room}")}
+        socket
+        |> put_flash(:info, "Room updated successfully")
+        |> push_navigate(to: ~p"/rooms/#{room}")
+        |> noreply()
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign_form(socket, changeset)}
+        socket |> assign_form(changeset) |> noreply()
     end
   end
 end
