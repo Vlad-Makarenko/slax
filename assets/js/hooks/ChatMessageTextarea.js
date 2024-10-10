43181e4a4019d9b1c87ebd59c3c47ef04c572e9d
Vlad-Makarenko <makrenkovladok@gmail.com>
2024-10-10 16:24:04 +0300

    improve threads with hooks

--- assets/js/hooks/ChatMessageTextarea.js
+++ assets/js/hooks/ChatMessageTextarea.js
@@ -2,7 +2,7 @@ export default ChatMessageTextarea = {
 	mounted() {
 		this.el.addEventListener('keydown', e => {
 			if (e.key === 'Enter' && !e.shiftKey) {
-				const form = document.getElementById('new-message-form');
+				const form = this.el.closest("form");
 
 				form.dispatchEvent(new Event('change', { bubbles: true, cancelable: true }));
 				form.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }));
